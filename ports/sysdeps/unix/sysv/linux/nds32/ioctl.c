befd92f0f843a9f6d68b848446f97b9f118a913e
vincentc <vincentc@andestech.com>
2015-12-31 16:42:20 +0800

    [Bug 11741] Boot kernel failed: getty: ttyS0: tcsetpgrp: Bad address in v3f toolcahin

---
+++ ports/sysdeps/unix/sysv/linux/nds32/ioctl.c
@@ -0,0 +1,17 @@
+#include <errno.h>
+#include <stdarg.h>
+#include <sysdep.h>
+
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
+int __ioctl (int __fd, unsigned long int __request, ...)
+{
+	unsigned long arg1;
+	va_list arg;
+	va_start (arg, __request);
+	arg1 = va_arg (arg, unsigned long);
+	va_end (arg);
+	return INLINE_SYSCALL(ioctl,3,__fd,__request,arg1);
+}
+weak_alias (__ioctl, ioctl)
+libc_hidden_weak (ioctl)
---
+++ ports/sysdeps/unix/sysv/linux/nds32/syscall.c
@@ -0,0 +1,29 @@
+#include <errno.h>
+#include <stdarg.h>
+#include <sysdep.h>
+long int syscall (long int __sysno, ...)
+{
+
+	int result;
+	unsigned long arg1,arg2,arg3,arg4,arg5,arg6;
+	va_list arg;
+	va_start (arg, __sysno);
+	arg1 = va_arg (arg, unsigned long);
+	arg2 = va_arg (arg, unsigned long);
+	arg3 = va_arg (arg, unsigned long);
+	arg4 = va_arg (arg, unsigned long);
+	arg5 = va_arg (arg, unsigned long);
+	arg6 = va_arg (arg, unsigned long);
+	va_end (arg);
+	__asm__ volatile(
+	"push\t	%0\n\t"
+	:
+	:"r"(arg6)
+	);
+	result = INLINE_SYSCALL(syscall,6,__sysno,arg1,arg2,arg3,arg4,arg5);
+	__asm__ volatile(
+	"pop\t	%0\n\t"
+	:"=r"(arg6)
+	);
+	return result;
+}
