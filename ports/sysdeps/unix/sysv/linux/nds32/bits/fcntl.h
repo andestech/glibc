8cc9d9195b889ffaedd7b9dc8e297f2a63f49737
vincentc <vincentc@andestech.com>
2016-01-11 13:10:28 +0800

    [Bug10807]Change 16 macros's definition in fcntl.h

--- ports/sysdeps/unix/sysv/linux/nds32/bits/fcntl.h
+++ ports/sysdeps/unix/sysv/linux/nds32/bits/fcntl.h
@@ -24,47 +24,6 @@
 
 #include <sys/types.h>
 
-/* open/fcntl - O_SYNC is only implemented on blocks devices and on files
-   located on an ext2 file system */
-#define O_APPEND	0x0008
-#define O_SYNC		0x4010
-#define O_NONBLOCK	0x0080
-#define O_CREAT		0x0100	/* not fcntl */
-#define O_TRUNC		0x0200	/* not fcntl */
-#define O_EXCL		0x0400	/* not fcntl */
-#define O_NOCTTY	0x0800	/* not fcntl */
-#define O_ASYNC		0x1000
-
-#define __O_DIRECT       0x8000 /* Direct disk access hint.  */
-#define __O_DSYNC        0x0010 /* Synchronize data.  */
-
-
-#ifdef __USE_LARGEFILE64
-# define __O_LARGEFILE  0x2000	/* Allow large file opens.  */
-#endif
-
-
-
-
-#ifndef __USE_FILE_OFFSET64
-# define F_GETLK	14	/* Get record locking info.  */
-# define F_SETLK	6	/* Set record locking info (non-blocking).  */
-# define F_SETLKW	7	/* Set record locking info (blocking).	*/
-#else
-# define F_GETLK	F_GETLK64  /* Get record locking info.	*/
-# define F_SETLK	F_SETLK64  /* Set record locking info (non-blocking).*/
-# define F_SETLKW	F_SETLKW64 /* Set record locking info (blocking).  */
-#endif
-
-#define F_GETLK64	33	/* Get record locking info.  */
-#define F_SETLK64	34	/* Set record locking info (non-blocking).  */
-#define F_SETLKW64	35	/* Set record locking info (blocking).	*/
-
-#if defined __USE_BSD || defined __USE_UNIX98
-# define __F_SETOWN	24	/* Get owner of socket (receiver of SIGIO).  */
-# define __F_GETOWN	23	/* Set owner of socket (receiver of SIGIO).  */
-#endif
-
 typedef struct flock
   {
     short int l_type;	/* Type of lock: F_RDLCK, F_WRLCK, or F_UNLCK.	*/
